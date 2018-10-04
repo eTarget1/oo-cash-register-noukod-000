f0d9f3b9192c7ed28f8d53900ab4cafb35dc38e8
clifford <clifford@noukod.com>
2018-10-04 18:36:44 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
  #   end
  #   self.last_transaction = amount * quantity
  # end
- # 
+ #
  # def apply_discount
  #    if discount != 0
  #      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
@@ -23,40 +23,40 @@ class CashRegister
  #      "There is no discount to apply."
  #    end
  #  end
- # 
+ #
  #  def void_last_transaction
  #    self.total = self.total - self.last_transaction
  #  end
 
- def add_item(title,price,quantity = 1)
- if quantity>1
-   i=0
-   while i<quantity
+   def add_item(title,price,quantity = 1)
+   if quantity>1
+     i=0
+     while i<quantity
+       @items << title
+       i+=1
+     end
+   else
      @items << title
-     i+=1
    end
- else
-   @items << title
- end
- @total += price*quantity
- @last_transaction_amount = @total
- @total
-end
+   @total += price*quantity
+   @last_transaction_amount = @total
+   @total
+  end
 
-def apply_discount()
- if @discount > 0
-   @discount = @discount/100.to_f
-   @total = @total - (@total * (@discount))
-   "After the discount, the total comes to $#{@total.to_i}."
- else
-   "There is no discount to apply."
- end
-end
+  def apply_discount()
+   if @discount > 0
+     @discount = @discount/100.to_f
+     @total = @total - (@total * (@discount))
+     "After the discount, the total comes to $#{@total.to_i}."
+   else
+     "There is no discount to apply."
+   end
+  end
 
 
-def void_last_transaction()
- @total -= @last_transaction_amount
-end
+  def void_last_transaction()
+   @total -= @last_transaction_amount
+  end
 
-
-end
+    
+  end
